695279f70d7a5357969b104916a29256dd6faa0f
R.Adhithya SRMIST <139868877+RAdhithya02@users.noreply.github.com>
2023-10-06 22:40:28 +0530

    Add files via upload

---
+++ Implementation of Linked List.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define a structure for a linked list node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert a node at the beginning of the list
+struct Node* insertAtBeginning(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+    newNode->next = head;
+    return newNode;
+}
+
+// Function to insert a node at the end of the list
+struct Node* insertAtEnd(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+    if (head == NULL) {
+        return newNode;
+    }
+    struct Node* current = head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    current->next = newNode;
+    return head;
+}
+
+// Function to delete a node with a given value
+struct Node* deleteNode(struct Node* head, int data) {
+    if (head == NULL) {
+        return NULL;
+    }
+    if (head->data == data) {
+        struct Node* temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+    struct Node* current = head;
+    while (current->next != NULL && current->next->data != data) {
+        current = current->next;
+    }
+    if (current->next == NULL) {
+        printf("Element not found in the list.\n");
+        return head;
+    }
+    struct Node* temp = current->next;
+    current->next = current->next->next;
+    free(temp);
+    return head;
+}
+
+// Function to print the linked list
+void printList(struct Node* head) {
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+// Function to free the memory used by the linked list
+void freeList(struct Node* head) {
+    struct Node* current = head;
+    while (current != NULL) {
+        struct Node* temp = current;
+        current = current->next;
+        free(temp);
+    }
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    // Insert elements at the beginning
+    head = insertAtBeginning(head, 10);
+    head = insertAtBeginning(head, 20);
+    head = insertAtBeginning(head, 30);
+
+    // Print the linked list
+    printf("Linked List after insertions at the beginning:\n");
+    printList(head);
+
+    // Insert elements at the end
+    head = insertAtEnd(head, 40);
+    head = insertAtEnd(head, 50);
+
+    // Print the linked list
+    printf("Linked List after insertions at the end:\n");
+    printList(head);
+
+    // Delete an element
+    head = deleteNode(head, 20);
+
+    // Print the linked list after deletion
+    printf("Linked List after deletion:\n");
+    printList(head);
+
+    // Free memory
+    freeList(head);
+
+    return 0;
+}
